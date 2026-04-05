928f6fa1c6ed7d4c0bfbacd92cd08313c6a13f5e
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2026-04-05 10:45:06 +0300

    refactor: simplify linting attributes in ffi.rs

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,6 +1,3 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(unsafe_op_in_unsafe_fn)]
+#![allow(warnings)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
